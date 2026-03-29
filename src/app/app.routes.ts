3056b884b9ae3051b895a6ae9588e13d5f1f41e2
vimihh <vimihh@ttu.ee>
2026-03-29 14:33:32 +0300

    Add Limits page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'limits',
+    loadComponent: () => import('./pages/limits/limits.component').then((m) => m.LimitsComponent),
+  },
+  { path: '', redirectTo: 'limits', pathMatch: 'full' },
+];
